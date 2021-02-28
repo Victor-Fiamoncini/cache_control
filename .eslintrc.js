bfed60a345ea668669a926928eae5e2e25921d81
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-02-28 05:23:41 -0300

    chore: add deps & general configs

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	globals: {
+		module: true,
+		exports: true,
+		require: true,
+		process: true,
+		__dirname: true,
+	},
+	plugins: ['@typescript-eslint'],
+	rules: {
+		indent: ['error', 'tab', { SwitchCase: 1 }],
+		'linebreak-style': ['error', 'unix'],
+		quotes: ['error', 'single'],
+		semi: ['error', 'never'],
+		'@typescript-eslint/no-unused-vars': 'off',
+		'@typescript-eslint/ban-types': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-namespace': 'off',
+	},
+}
