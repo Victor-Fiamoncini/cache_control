98cf488a749d8a98bfb30a6865617bfdeac004fe
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-02-28 06:01:42 -0300

    feat: should delete old cache on sut.save

--- src/data/services/LocalSavePurchasesService.spec.ts
+++ src/data/services/LocalSavePurchasesService.spec.ts
@@ -1,11 +1,21 @@
 class LocalSavePurchasesService {
 	constructor(private readonly cacheStore: CacheStore) {}
+
+	async save(): Promise<void> {
+		this.cacheStore.delete()
+	}
 }
 
-interface CacheStore {}
+interface CacheStore {
+	delete(): void
+}
 
 class CacheStoreSpy implements CacheStore {
 	deleteCallsCount = 0
+
+	delete(): void {
+		this.deleteCallsCount++
+	}
 }
 
 describe('LocalSavePurchasesService', () => {
@@ -15,5 +25,15 @@ describe('LocalSavePurchasesService', () => {
 		new LocalSavePurchasesService(cacheStore)
 
 		expect(cacheStore.deleteCallsCount).toBe(0)
+	})
+
+	test('should delete old cache on sut.save', async () => {
+		const cacheStore = new CacheStoreSpy()
+
+		const sut = new LocalSavePurchasesService(cacheStore)
+
+		await sut.save()
+
+		expect(cacheStore.deleteCallsCount).toBe(1)
 	})
 })
