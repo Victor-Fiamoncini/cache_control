14fe24b5db29f08423bcb0cd27837c78c0f7468e
Victor-Fiamoncini <victor.fiamoncini@gmail.com>
2021-02-28 05:57:57 -0300

    feat: ensure LocalSavePurchasesService does not delete cache on init

--- src/data/services/LocalSavePurchasesService.spec.ts
+++ src/data/services/LocalSavePurchasesService.spec.ts
@@ -1,5 +1,19 @@
+class LocalSavePurchasesService {
+	constructor(private readonly cacheStore: CacheStore) {}
+}
+
+interface CacheStore {}
+
+class CacheStoreSpy implements CacheStore {
+	deleteCallsCount = 0
+}
+
 describe('LocalSavePurchasesService', () => {
-	test('should ', () => {
-		expect(1).toBe(1)
+	test('should not delete cache on sut.init', () => {
+		const cacheStore = new CacheStoreSpy()
+
+		new LocalSavePurchasesService(cacheStore)
+
+		expect(cacheStore.deleteCallsCount).toBe(0)
 	})
 })
